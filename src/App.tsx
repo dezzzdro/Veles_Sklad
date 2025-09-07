2460b27d6f28b732755790c9e5ae6fa5c6018dbb
dezzzdro <dezzzdro@gmail.com>
2025-09-07 08:33:05 +0300

    fix: Restore full application functionality after MIME type fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
+import Layout from './layouts/MainLayout'
+import Dashboard from './pages/Dashboard'
+import Warehouse from './pages/Warehouse'
+import Assembly from './pages/Assembly'
+import Arrival from './pages/Arrival'
+import Issued from './pages/Issued'
+import Counterparties from './pages/Counterparties'
+import Settings from './pages/Settings'
+import Notifications from './pages/Notifications'
+import Debug from './pages/Debug'
+import UIDemo from './components/UIDemo'
+import { useThemeStore } from './stores/themeStore'
+
 function App() {
+  const { initializeTheme } = useThemeStore()
+
+  useEffect(() => {
+    initializeTheme()
+  }, [initializeTheme])
+
   return (
-    <div>
-      <h1>Велес Склад - Современная версия</h1>
-      <p>Приложение загружено успешно! Модули работают корректно.</p>
-    </div>
+    <>
+      <Toaster position="top-right" />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/sklad" element={<Warehouse />} />
+          <Route path="/sborka" element={<Assembly />} />
+          <Route path="/prihod" element={<Arrival />} />
+          <Route path="/vydannoe" element={<Issued />} />
+          <Route path="/kontragenty" element={<Counterparties />} />
+          <Route path="/nastroyki" element={<Settings />} />
+          <Route path="/uvedomleniya" element={<Notifications />} />
+          <Route path="/otladka" element={<Debug />} />
+          <Route path="/ui-demo" element={<UIDemo />} />
+        </Routes>
+      </Layout>
+    </>
   )
 }
 
